b18803df1bc64678ea57504086b8b4f72fab3160
cydiralis <cydiralis@proton.me>
2025-04-06 19:18:50 +0100

    monado works with an asterisk, systemd service is inexplicably missing

--- base/monado.nix
+++ base/monado.nix
@@ -10,7 +10,7 @@
     enable = true;
     defaultRuntime = true; # Register as default OpenXR runtime
   };
- 
+
   systemd.user.services.monado.environment = {
     STEAMVR_LH_ENABLE = "1";
     XRT_COMPOSITOR_COMPUTE = "1";
